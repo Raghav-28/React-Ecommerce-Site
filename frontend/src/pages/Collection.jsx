73df81ebbb7c69307282b2f3b02fb36ec008813e
Raghav-28 <kraghavgupta@gmail.com>
2024-12-23 14:31:57 +0530

    Seach functionality Completed

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,12 +5,14 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-  const {products} = useContext(ShopContext);
+  const {products , search , showSearch} = useContext(ShopContext);
   const [showFilter,setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category , setCategory] =useState([]);
   const [subCategory , setSubCategory]= useState([]);
   const [sortType, setSortType]= useState('relevant');
+
+
 
   const toggleCategory =(e)=>{
 
@@ -33,6 +35,10 @@ const Collection = () => {
 
   const applyFilter = () =>{
     let productsCopy = products.slice();
+
+    if(showSearch && search){
+      productsCopy = productsCopy.filter(item=> item.name.toLowerCase().includes(search.toLowerCase()));
+    }
 
     if(category.length >0){
       productsCopy= productsCopy.filter(item => category.includes(item.category));
@@ -62,7 +68,7 @@ const Collection = () => {
     
   useEffect(()=>{
     applyFilter();
-  },[category , subCategory])
+  },[category , subCategory , search, showSearch])
 
 useEffect (()=>{
   sortProducts();
